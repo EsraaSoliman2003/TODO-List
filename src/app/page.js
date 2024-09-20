fd43b8829020a8e132a36bab222af66c4e0c0309
Esraa Soliman <ee341110@gmail.com>
2024-09-20 14:52:11 +0300

    first commit

--- src/app/page.js
+++ src/app/page.js
@@ -1,11 +1,13 @@
-"use client"
+"use client";
 import Link from "next/link";
 import styles from "./page.module.css";
 import { useSession, signIn, signOut } from "next-auth/react";
+import { useRouter } from "next/navigation";
 
 export default function Home() {
+  const router = useRouter();
   const session = useSession();
-  console.log(session)
+  console.log(session);
   // if (session) {
   //   return (
   //     <>
@@ -25,6 +27,7 @@ export default function Home() {
         <Link className={styles.link} href="/register">
           Register
         </Link>
+        {session.status === "authenticated" && router.push("/tasksManagement")}
       </div>
     </div>
   );
